ecdee1d1448a17d919a1dd88ed6126925b99a762
Martin Danek <martindanek4@gmail.com>
2021-01-22 17:40:19 +0100

    text cleaning

--- text_analyzer_object.py
+++ text_analyzer_object.py
@@ -11,37 +11,56 @@ import string
 import os
 
 
-class TextCleaner:
+class TextAnalyzer:
 
     def __init__(self, file_path: str, clean_file_name='clean_text.txt'):
-        self._full_path = file_path if os.path.isfile(file_path) else AttributeError
+        if os.path.isfile(file_path):
+            self._full_path = file_path
+        else:
+            print('wrong path or file name')
+            exit()
         self.dir_path = os.path.dirname(file_path)
         self.new_name = clean_file_name
         self._raw_text = self.file_opener()
-        self._clean_text = self.text_cleaner()
+        self.clean_text = self.text_cleaner()
+        self._result_file = self.top_words()
 
     def file_opener(self):
         with open(self._full_path, 'r') as file:
             self._raw_text = file.read()
         return self._raw_text
 
+    def file_writer(self):
+        with open(self.dir_path + self.new_name, 'w') as file:
+            file.write(''.join(self._result_file))
+
     def text_cleaner(self):
-        self._clean_text = [word.strip(string.punctuation) for word in self._raw_text.split()]
-        return self._clean_text
+        self.clean_text = [word.strip(string.punctuation + '\n').lower()
+                           for word in self._raw_text.split() if len(word) > 4
+                           ]
+        return self.clean_text
 
-    def most_frekquent_words(self):
-        my_dict, top_list = {}, []
+    def top_words(self, top_x=3):
+        my_dict, self._result_file = {}, []
 
-        for word in self._clean_text:
+        for word in self.clean_text:
             if word not in my_dict:
                 my_dict[word] = 1
             else:
                 my_dict[word] += 1
 
-        top_list_values = sorted(my_dict.values())[:4]
+        top_list_values = sorted(my_dict.values())[:top_x]
 
         for word in my_dict:
             if my_dict[word] in top_list_values:
-                top_list.append(word)
-        return top_list
+                self._result_file.append(word)
+        return self._result_file
 
+
+if __name__ == '__main__':
+    trvalky = TextAnalyzer('/Users/martindanek/Downloads/raw_text.txt')
+    print(trvalky.dir_path)
+    print(trvalky.new_name)
+    print(trvalky.clean_text)
+    trvalky.top_words(5)
+    trvalky.file_writer()
