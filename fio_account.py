f2b6185e21e07fb93cd71cd1e40690a5e0380b95
Martin Danek <martindanek4@gmail.com>
2021-02-05 21:53:59 +0100

    PEP8

--- fio_account.py
+++ fio_account.py
@@ -1,8 +1,8 @@
 # ===== FIO transparent account scrapper =====
 
 import requests
-import bs4
 import csv
+import bs4
 
 
 def compose_url(account, date_from, date_to):
@@ -14,20 +14,30 @@ def compose_url(account, date_from, date_to):
 
 
 def write_data(arg, account):
-    with open(f'/Users/martindanek/Documents/programovani/files/csv/fio_{str(account)}.csv', 'w') as file:
+    with open(f'/Users/martindanek/Documents/programovani/files/csv/fio_'
+              f'{str(account)}.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerows(arg)
 
 
 def fio_account(account, date_from, date_to):
+
     url = compose_url(account, date_from, date_to)
     data = requests.get(url)
 
     soup = bs4.BeautifulSoup(data.text, 'html.parser')
 
-    header = [item.text for item in soup.find('table').find_next('table').find('thead').find_all('th')]
+    header = [
+        item.text
+        for item in
+        soup.find('table').find_next('table').find('thead').find_all('th')
+    ]
 
-    transaction = [item for item in soup.find('table').find_next('table').find('tbody').find_all('td')]
+    transaction = [
+        item
+        for item in
+        soup.find('table').find_next('table').find('tbody').find_all('td')
+    ]
 
     clean_data = []
     sub_list = []
