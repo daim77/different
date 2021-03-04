d67c5d443e70f50c613ed5bf28660018f5f1603a
Martin Danek <martindanek4@gmail.com>
2021-03-04 10:49:27 +0100

    repo content

--- API.py
+++ API.py
@@ -3,8 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
     return "Hello Flask!"
 
-app.run(port=2222)
+
+app.run(port=2222, debug=True)
--- server.py
+++ server.py
@@ -4,6 +4,10 @@ from flask import Flask
 SECRET_MESSAGE = 'Babis je idiot a dela veci jen JAKO'
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def get_secret_message():
     return SECRET_MESSAGE
+
+
+app.run(port=5683, debug=True)
