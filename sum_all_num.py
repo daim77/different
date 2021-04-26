f451d3d56d5f63c2ae1974b44c44ea94393f7222
Martin Danek <martindanek4@gmail.com>
2021-04-26 20:24:19 +0200

    sum even sci numer

--- sum_all_num.py
+++ sum_all_num.py
@@ -5,11 +5,12 @@ def sum_num(my_list):
     counter = 0
     my_list = str(my_list).lower()
     my_list = re.sub(r'true', 'x1x', my_list)
-    for i in re.findall(r"[+-]?\d+\.\d+|[+-]?\d+", my_list):
+    for i in re.findall(
+            r"[+-]?\d+\.\d+[e][+-]\d+|[+-]?\d+\.\d+|[+-]?\d+", my_list):
         counter += float(i)
     return counter
 
 
 if __name__ == '__main__':
-    data = [1, 's3True', '-4.5', 'true']
+    data = [1, 's3True', '-4.5', 'true', 1.1e6]
     print(sum_num(data))
