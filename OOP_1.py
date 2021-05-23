8e6c8cf4438650873969c4ea01b41d1993d4d96b
Martin Danek <martindanek4@gmail.com>
2021-05-23 10:47:56 +0200

    OOP_points

--- OOP_1.py
+++ OOP_1.py
@@ -22,5 +22,25 @@ class SmartFridge:
         )
 
 
+class Points:
+    counter = 0
+
+    def __init__(self, x, y):
+        self.x_value = x
+        self.y_value = y
+        Points.counter += 1
+
+    def distance(self, point2):
+        print(
+            f'Distance between '
+            f'{self} and '
+            f'{point2} is '
+            f'{((self.x_value - point2.x_value) ** 2 + (self.y_value - point2.y_value) ** 2) ** (1/2)}'
+        )
+
+
 my_fridge = SmartFridge('Bosch', 'ABCx001')
 office_fridge = SmartFridge('Miele', 'XYZx999')
+
+bod1 = Points(1, 1)
+bod2 = Points(2, 2)
