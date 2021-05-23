f0bc314d83f968e5c4b145f4aa23e7618f6aa900
Martin Danek <martindanek4@gmail.com>
2021-05-23 22:21:08 +0200

    OOP_phone

--- OOP_1.py
+++ OOP_1.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 
+# info about fridge
 class SmartFridge:
 
     fridge_counter = 0
@@ -22,6 +23,7 @@ class SmartFridge:
         )
 
 
+# Two points and distance
 class Points:
     counter = 0
 
@@ -38,6 +40,40 @@ class Points:
             f'{((self.x_value - point2.x_value) ** 2 + (self.y_value - point2.y_value) ** 2) ** (1/2)}'
         )
 
+
+# phone
+class Phone:
+    counter = 0
+
+    def __init__(self, brand: str, sn: str):
+        self.brand = brand
+        self.serial_number = sn
+        self.contact_dict = dict()
+        Phone.counter += 1
+
+    def add_contact(self, surname: str, name: str, phone_number: str):
+        if surname + ' ' + name in self.contact_dict:
+            print(f'{surname}, {name} contact is already in the list')
+            what_to_do = \
+                input('press 0 to skip action or 1 to overwrite contact')
+            if what_to_do == '0':
+                return
+            elif what_to_do == '1':
+                self.contact_dict.update({surname + ' ' + name: phone_number})
+                print(f'contact {surname}, {name}, {phone_number} saved')
+        self.contact_dict.update({surname + ' ' + name: phone_number})
+        print(f'contact {surname}, {name}, {phone_number} saved')
+
+    def del_contact(self, surname: str, name: str):
+        if self.contact_dict.pop(surname + ' ' + name, 'deleted') == 'deleted':
+            print(f'Contact {surname}, {name} not in the list')
+
+    def search_contact(self):
+        for key in self.contact_dict:
+            print(key, ':', self.contact_dict[key])
+
+
+iphone = Phone('iPhone', 'sn0001')
 
 my_fridge = SmartFridge('Bosch', 'ABCx001')
 office_fridge = SmartFridge('Miele', 'XYZx999')
