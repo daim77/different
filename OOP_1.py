c87438afcccafbcf7cfbcd41f2e942826779ee03
Martin Danek <martindanek4@gmail.com>
2021-05-23 10:00:38 +0200

    OOP_fridge

--- OOP_1.py
+++ OOP_1.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 class SmartFridge:
 
     fridge_counter = 0
@@ -5,10 +8,18 @@ class SmartFridge:
     def __init__(self, brand, serial_number):
         self.brand = brand
         self.serial_number = serial_number
+        self.f_date = date.today()  # factory date
         SmartFridge.fridge_counter += 1
 
     def greet(self):
         print(f'Hello! I am {self.brand}')
+
+    def age(self):
+        print(
+            f'Fridge {self.brand} '
+            f'serial number {self.serial_number} '
+            f'is {(date.today() - self.f_date).days} days old'
+        )
 
 
 my_fridge = SmartFridge('Bosch', 'ABCx001')
