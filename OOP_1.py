5c5047b0a005934af653f2a2311619856e014ae8
Martin Danek <martindanek4@gmail.com>
2021-05-23 09:43:43 +0200

    OOP_init

--- OOP_1.py
+++ OOP_1.py
@@ -1,8 +1,11 @@
 class SmartFridge:
 
+    fridge_counter = 0
+
     def __init__(self, brand, serial_number):
         self.brand = brand
         self.serial_number = serial_number
+        SmartFridge.fridge_counter += 1
 
     def greet(self):
         print(f'Hello! I am {self.brand}')
@@ -10,7 +13,3 @@ class SmartFridge:
 
 my_fridge = SmartFridge('Bosch', 'ABCx001')
 office_fridge = SmartFridge('Miele', 'XYZx999')
-
-
-
-
